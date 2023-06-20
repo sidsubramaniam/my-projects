11493f3529599e2f836d75fdadf2abc4a8c265c0
Siddharth Subramaniam <136026337+sidsubramaniam@users.noreply.github.com>
2023-06-20 13:42:29 +0530

    basic_calculator.py

--- basic_calculator.py
+++ basic_calculator.py
@@ -12,16 +12,16 @@ ev=str(input("to evaluate enter '=': "))
 if ev=="=":
     if op=="+":
         print(str(a)+op+str(b)+"="+str(a+b))
-        print("Thank you!")
+        print("THANK YOU!")
     elif op=="-":
         print(str(a)+op+str(b)+"="+str(a-b))
-        print("Thank you!")
+        print("THANK YOU!")
     elif op=="*":
         print(str(a)+op+str(b)+"="+str(a*b))
-        print("Thank you!")
+        print("THANK YOU!")
     elif op=="/":
         print(str(a)+op+str(b)+"="+str(a//b))
-        print("Thank you!")
+        print("THANK YOU!")
     else:
         print("invalid entry, please try again :(")
         
