41ee4a10fd7f0f17883d55ccf0e7986106ca19bc
Siddharth Subramaniam <136026337+sidsubramaniam@users.noreply.github.com>
2023-06-20 13:26:00 +0530

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,36 @@
+#Program to simulate a basic calculator
+
+print("*** SIMPLE CALCULATOR ***")
+print("this calculator can perform basic arithmetic operations (+,-,*,/)")
+a=float(input("Enter number = "))
+print(a)
+op=str(input("Enter operator: "))
+print(str(a) + op)
+b=float(input("Enter number = "))
+print(str(a) + op + str(b))
+ev=str(input("to evaluate enter '=': "))
+if ev=="=":
+    if op=="+":
+        print(str(a)+op+str(b)+"="+str(a+b))
+    elif op=="-":
+        print(str(a)+op+str(b)+"="+str(a-b))
+    elif op=="*":
+        print(str(a)+op+str(b)+"="+str(a*b))
+    elif op=="/":
+        print(str(a)+op+str(b)+"="+str(a//b))
+    else:
+        print("invalid entry, please try again :(")
+        
+else:
+    print("invalid entry, please try again :(")
+
+
+
+
+    
+
+
+
+         
+         
+
